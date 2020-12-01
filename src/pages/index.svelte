2fb7cf4283263c367b9bc1477192a9450dfe0197
mzaini30 <muhzaini30@gmail.com>
2020-12-01 17:34:25 +0800

    upload

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -6,14 +6,36 @@
 		<div class="col">
 			<div class="form-group">
 				<label>Hari Ujian</label>
-				<input type="tel" class="form-control" placeholder="1" name="">
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text">Ke</span>
+					</div>
+					<input type="tel" class="form-control" bind:value={hariUjian} placeholder="1" name="">
+				</div>
 			</div>
 		</div>
 		<div class="col">
 			<div class="form-group">
 				<label>Juz yang Diujikan</label>
-				<input type="tel" class="form-control" placeholder="1-5" name="">
+				<div class="input-group">
+					<input type="tel" class="form-control" bind:value={juz} placeholder="5" name="">
+					<div class="input-group-append">
+						<span class="input-group-text">Juz</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
+	<div class="d-flex justify-content-between">
+		<div>
+			<button class="btn btn-warning" on:click={() => juz = ''}>Reset</button>
+		</div>
+		<div>
+			<button class="btn btn-success">Mulai</button>
+		</div>
+	</div>
 </div>
+<script type="text/javascript">
+	let hariUjian = ''
+	let juz = ''
+</script>
